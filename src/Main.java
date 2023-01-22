b1e44cc70c4dda90d60bcd4c1309246f223ef63e
eishi1103 <78992733+eishi1103@users.noreply.github.com>
2023-01-22 21:48:21 +0900

    少し修正
    コメントを加えました

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		Phone Galaxy = new Phone("samsung", "GalaxyZ", 15.8, true);
-		//ゲッターが動いているか確認
+		//Galaxyを試しにインスタンス化してゲッターが動いているか確認
 		System.out.println("あなたの携帯は" + Galaxy.getMaker() + "の" + Galaxy.getModel() + "です。");
 
+		//iPhoneをインスタンす化、作ったメソッドを試してみる。
 		Phone iPhone = new Phone("apple", "iPhone14", 14.7, false);
 		iPhone.call();
 		iPhone.turnOn();
